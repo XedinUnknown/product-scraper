d15cfe55cc3b8ab1c16c381dd82a8d587b8e4b85
Anton Ukhanev <xedin.unknown@gmail.com>
2022-10-18 03:21:40 +0000

    Add method description

--- src/product/ProductScraper.ts
+++ src/product/ProductScraper.ts
@@ -3,5 +3,8 @@ import Page from 'common/Page';
 import Optional from 'common/Optional';
 
 export default interface ProductScraper {
+  /**
+   * Scrapes a product from a page it appears on.
+   */
   (productPage: Page, defaults?: Optional<Product>): Product;
 }
